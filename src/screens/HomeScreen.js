bed1a09dc1bb1f82303745173299e7241fbad322
rash0924 <127588221+rash0924@users.noreply.github.com>
2026-01-03 09:14:31 +0530

    Create HomeScreen.js

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient';
+import { COLORS, SIZES } from '../constants/Theme';
+import { Leaf, CloudRain, Sun, Wind } from 'lucide-react-native';
